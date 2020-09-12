e0043a0f329015a3c5dd98db859a5a5bc021264b
jesseandersonumd <57415941+jesseandersonumd@users.noreply.github.com>
2020-09-12 16:09:00 -0400

    Create songlyrics.py

---
+++ songlyrics.py
@@ -0,0 +1,37 @@
+import requests
+import bs4
+import pandas as pd
+import re
+
+df = pd.read_csv("songs_assigned_to_jander19.csv")
+
+def clean(data, column):  
+    cleaned_list = []
+    for x in data[column]:
+        x = re.sub("[-'.&+]", '', x)
+        x = '-'.join(x.split())
+        cleaned_list.append(x.lower())
+    return cleaned_list
+
+performers = clean(df,'Performer')
+lyrics = clean(df,'Song')
+
+url_list = []
+
+songs_and_performers = list(zip(performers,lyrics))
+
+for x,y in songs_and_performers:
+    url = f'http://www.songlyrics.com/{x}/{y}-lyrics/'
+    url_list.append(url)
+    
+songs_and_performers = list(zip(performers,lyrics,url_list))
+
+for x, y, z in songs_and_performers:
+    chosen_url = z
+    title = x+y
+    result = requests.get(chosen_url)
+    soup = bs4.BeautifulSoup(result.text,'lxml')
+    
+    with open(f'{title}.txt','w') as songs:
+        for line in soup.select('#songLyricsDiv')[0].get_text().split('\n'):
+            songs.write(line)
