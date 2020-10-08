aa0792184395c132801174110b6be372b9c5f307
jesseandersonumd <57415941+jesseandersonumd@users.noreply.github.com>
2020-10-08 17:09:30 -0400

    Add files via upload

--- songlyrics.py
+++ songlyrics.py
@@ -2,8 +2,9 @@ import requests
 import bs4
 import pandas as pd
 import re
+import os
 
-df = pd.read_csv("songs_assigned_to_jander19.csv")
+df = pd.read_csv("songs_assigned_to_rchatte2.csv")
 
 def clean(data, column):  
     cleaned_list = []
@@ -15,14 +16,14 @@ def clean(data, column):
 
 performers = clean(df,'Performer')
 lyrics = clean(df,'Song')
-songs_and_performers = list(zip(performers,lyrics,[songid for songid in df['SongID']]))
+songs_and_performers = list(zip(performers,lyrics,[re.sub("[-'.&?)(+/\"]", '', songid) for songid in df['SongID']]))
 url_list = []
 
 for x,y,z in songs_and_performers:
     url = f'http://www.songlyrics.com/{x}/{y}-lyrics/'
     url_list.append(url)
     
-songs_and_performers = list(zip(performers,lyrics,[songid for songid in df['SongID']],url_list))
+songs_and_performers = list(zip(performers,lyrics,[re.sub("[-'.&?)(+/\"]", '', songid) for songid in df['SongID']],url_list))
 
 for w, x, y, z in songs_and_performers:
     chosen_url = z
@@ -31,5 +32,10 @@ for w, x, y, z in songs_and_performers:
     soup = bs4.BeautifulSoup(result.text,'lxml')
     
     with open(f'{title}.txt','w') as songs:
-        for line in soup.select('#songLyricsDiv')[0].get_text().split('\n'):
-            songs.write(line + '\n')
+        try:
+            for line in soup.select('#songLyricsDiv')[0].get_text().split('\n'):
+                songs.write(line + '\n')
+        except:
+            songs.write("Song lyrics do not exist");
+            print(title)
+            continue;
